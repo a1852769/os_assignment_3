ac2314796bd317bfef8585bf06e34a407beec258
a1852769 <a1852769@adelaide.edu.au>
2025-10-27 16:51:48 +1030

    Serial mergesort implemented

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -9,10 +9,24 @@
 
 /* this function will be called by mergesort() and also by parallel_mergesort(). */
 void merge(int leftstart, int leftend, int rightstart, int rightend){
+
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
-void my_mergesort(int left, int right){
+void my_mergesort(int left, int right){ //Just a regular serial mergesort algorithm 
+	int middle;
+	//Base case: when there's 1 element
+	if (left >= right) {
+		return;
+	}
+
+	//Otherwise call mergesort on the left and right half 
+	middle = (left + right)/2; //should floor, making left smaller than right if odd 
+	my_mergesort(left, middle);
+	my_mergesort(middle+1, right);
+
+	//Merge the two sorted sub arrays
+	merge(left, middle, middle+1, right);
 }
 
 /* this function will be called by the testing program. */
@@ -22,6 +36,7 @@ void * parallel_mergesort(void *arg){
 
 /* we build the argument for the parallel_mergesort function. */
 struct argument * buildArgs(int left, int right, int level){
+
 		return NULL;
 }
 
